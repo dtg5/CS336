be584f3c1faa272af00787c4a8e5936b3a065aac
dtg5 <dtg5@users.noreply.github.com>
2018-09-19 14:48:07 -0400

    Update app.js

--- lab03/app.js
+++ lab03/app.js
@@ -4,8 +4,6 @@ const port = 3000
 
 app.get('/', (req, res) => res.redirect("http://localhost:3000/index.html"));
 
-// app.listen(port, () => console.log('My app is listening on port ${port}!'))
-
 app.listen(3000, function () {
   console.log('My app is listening on port 3000!');
 });
