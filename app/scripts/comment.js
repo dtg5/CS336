50612e80dc0e3527035b3f5c155482d927a2043a
dtg5 <dtg5@users.noreply.github.com>
2018-12-18 23:59:56 -0500

    Add files via upload

---
+++ app/scripts/comment.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import Remarkable from 'remarkable';
+
+module.exports = React.createClass({
+  rawMarkup: function() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+},
+
+  render: function() {
+    var md = new Remarkable();
+    return (
+      <div className="Comment">
+        <h2 className="CommentAuthor">
+          {this.props.author}
+        </h2>
+        <Link to={'/' + this.props.id}>Edit</Link>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
